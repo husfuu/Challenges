05ee4dbc97ec829c2a3471d83598e8d9a8691286
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2021-11-27 03:39:52 +0800

    desctructing assignment to extract values from an object

---
+++ freeCodeCamp/JS Algorithm and Data Structures/ES6/09_destructing_assignment2extract_values_from_object.js
@@ -0,0 +1,21 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  // const today = HIGH_TEMPERATURES.today;
+  // const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  const {today, yesterday, tomorrow} = HIGH_TEMPERATURES
+  
+  console.log(today)
+  console.log(yesterday)
+  console.log(tomorrow)
+  
+  // outputs
+  // 77
+  // 75
+  // 80
